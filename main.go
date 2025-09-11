6101c686ce063ec1380191b482a03084b2225922
mahshin1428 <bsse1428@iit.du.ac.bd>
2025-09-11 13:05:38 +0600

    function types

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
+
 import (
 	"fmt"
 	"tiny-gopher/mathLib"
 )
 
+
+// execution order: global--> init() -> main() -> other functions
+// functions types
+
+//1.named  function
 func printHi(){
 	fmt.Println("Hello")
 }
@@ -13,4 +19,16 @@ func main(){
 	printHi()
 	result := mathLib.Sum(2,4)
 	fmt.Println("Sum is:", result)
+
+	//3. annonymous function
+	func(a int, b int){
+		c:= a + b
+		fmt.Println("Sum from annonymous function:", c)
+	}(3,5)  //immediatly invoke function expression(IIFE)
 }
+
+// 2.init function
+func init(){
+	fmt.Println("Init function called")
+}
+
