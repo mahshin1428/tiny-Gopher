c01ec0b1760f52b5ddf19c1450fe3934872b10f8
mahshin1428 <bsse1428@iit.du.ac.bd>
2025-09-12 16:15:16 +0600

    Closure function

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 
 import (
 	"fmt"
-	"tiny-gopher/mathLib"
+	//"tiny-gopher/mathLib"
 )
 
 
@@ -11,8 +11,15 @@ import (
 // functions types
 
 //1.named  function
+/*
+
 func printHi(){
 	fmt.Println("Hello")
+}
+
+// 2.init function
+func init(){
+	fmt.Println("Init function called")
 }
 
 func main(){
@@ -26,9 +33,41 @@ func main(){
 		fmt.Println("Sum from annonymous function:", c)
 	}(3,5)  //immediatly invoke function expression(IIFE)
 }
+*/
 
-// 2.init function
-func init(){
-	fmt.Println("Init function called")
+
+
+//closure function
+
+const a = 10
+
+var p = 20
+
+func outer() func(){
+	money := 100
+	age := 30
+	fmt.Println("age is", age)
+
+	show:= func(){
+		money = money + a + p
+		fmt.Println("money is:", money)
+	}
+	return show // doubt silo function ki return korbe? (ans: Go-তে function নিজেও একটা value)
+}
+
+func call(){
+	innerfunc := outer()
+	innerfunc()   //130
+	innerfunc()   //160
+	innerfunc()   //190
+
+	innerfunc2 := outer()
+	innerfunc2()  //130
+	innerfunc2()  //160
+	innerfunc()   //220
+}
+
+func main(){
+	call()
 }
 
